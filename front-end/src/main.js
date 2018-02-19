232ff0549c54c215f444e6ffc8e6fcfb132b2f9a
Morrier-Bompoint Thomas <thomasmorrier@gmail.com>
2018-02-19 18:22:19 -0500

    Remove custom template

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,13 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
-import router from './router';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import App from './App';
+import router from './router';
 
 Vue.use(Vuetify, { theme: {
-  primary: '#ee44aa',
+  primary: '#f37d37',
   secondary: '#424242',
   accent: '#82B1FF',
   error: '#FF5252',
---
+++ front-end/src/pages/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-container fluid>
+    <h1>Beeye front-end challenge</h1>
+  </v-container>
+</template>
+
+<style scoped>
+</style>
